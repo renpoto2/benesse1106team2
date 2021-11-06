cf963364df729f200d80239b63528d6fa5fca8fe
masaki-lab <62365142+masaki-lab@users.noreply.github.com>
2021-11-06 22:47:30 +0900

    Update testPage.dart

--- lib/pages/testPage.dart
+++ lib/pages/testPage.dart
@@ -1,6 +1,23 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-class testScreen extends StatelessWidget {
+
+class testScreen extends StatefulWidget {
+@override
+_testScreen createState() => _testScreen();
+}
+
+
+class _testScreen extends State<testScreen> {
   // This widget is the root of your application.
+  int _counter = 0;
+  List<String>titleList = ['国語','数学','英語','理科','社会'];
+  void _incrementCounter() {
+    setState(() {
+
+      _counter++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,10 +26,30 @@ class testScreen extends StatelessWidget {
         appBar: AppBar(
           backgroundColor: Colors.blue[100],
           title: Text(
-            'Cloud',
-            style: TextStyle(fontSize: 16),
+            'Subject',
+            style: TextStyle(fontSize: 20),
           ),
         ),
-        body: Center(child: Icon(Icons.cloud)));
+        body: ListView.builder(
+            itemCount: titleList.length,
+            itemBuilder: (BuildContext context, int i){
+              return Column(
+                children: <Widget>[
+                 ListTile(
+                  onTap: (){
+
+                  },
+                   title:  Row(children: <Widget>[new Text(titleList[i],
+                     style:  TextStyle(
+                         fontWeight: FontWeight.w500, fontSize: 25.0),)
+                   ], mainAxisAlignment: MainAxisAlignment.center,),
+
+                ),
+                Divider(thickness: 1,),
+              ],
+            );
+          },
+        ),
+     );
   }
 }
