f1f8c7136825f5af190edf75df818e32ce07a0d9
renpoto2 <renpoto2@icloud.com>
2021-11-06 16:28:43 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/benesse1106team2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.benesse1106team2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:benesse1106team2/pages/nigatePage.dart';
+import 'package:benesse1106team2/pages/testPage.dart';
+import 'package:flutter/material.dart';
+
+import 'pages/schedulePage.dart';
+
+void main() => runApp(MyApp());
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BottomNav',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+class _MyHomePageState extends State<MyHomePage> {
+  // ページインデックス保存用
+  int _screen = 0;
+  // 表示する Widget の一覧
+  static List<Widget> _pageList = [
+    scheduleScreen(),
+    testScreen(),
+    nigateSchreen()
+  ];
+  // ページ下部に並べるナビゲーションメニューの一覧
+  List<BottomNavigationBarItem> myBottomNavBarItems() {
+    return [
+      BottomNavigationBarItem(
+        icon: Icon(Icons.book),
+        label: 'Book',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.cloud),
+        label: 'Cloud',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.cake),
+        label: 'Cake',
+      ),
+    ];
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // Appbar
+
+      // ページビュー
+      body: _pageList[_screen],
+      // ページ下部のナビゲーションメニュー
+      bottomNavigationBar: BottomNavigationBar(
+        // 現在のページインデックス
+        currentIndex: _screen,
+        // onTapでナビゲーションメニューがタップされた時の処理を定義
+        onTap: (index) {
+          setState(() {
+            // ページインデックスを更新
+            _screen = index;
+          });
+        },
+        // 定義済のナビゲーションメニューのアイテムリスト
+        items: myBottomNavBarItems(),
+      ),
+    );
+  }
+}
